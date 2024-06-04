1aab267376c7447018d0135ba98d0592c4c2cd8c
Daniel Berihun <danielshitaye10@gmail.com>
2024-06-04 06:47:10 -0700

    first commit on Binary Tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: a pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: a pointer to the new node or NULL if it fails to create
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	/* allocating memory for the new node */
+
+	if (new == NULL)
+		return (NULL); /* malloc fails */
+
+	new->n = value;
+	new->parent = parent;
+
+	return (new);
+}

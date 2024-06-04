9682687f201845164b467056ddebff7cdecbe749
Daniel Berihun <danielshitaye10@gmail.com>
2024-06-04 07:43:51 -0700

    free new if parent null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	if (new == NULL)
 		return (NULL);
